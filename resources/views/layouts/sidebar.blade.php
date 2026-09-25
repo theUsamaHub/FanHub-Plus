7fe5b5806c0511d5fb1884f702e4ab896e6afe3d
theUsamaHub <usamabusiness2025@outlook.com>
2026-09-25 22:11:48 +0500

    Keep contacts and subscribers commented out in sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -49,12 +49,12 @@
         <a class="fh-adm-nav-link {{ request()->routeIs('admin.roles.*') ? 'is-active' : '' }}" href="{{ route('admin.roles.index') }}" title="{{ __('Roles') }}">
             <i class="bi bi-shield-lock"></i> <span>{{ __('Roles') }}</span>
         </a>
-        <a class="fh-adm-nav-link {{ request()->routeIs('admin.contacts.*') ? 'is-active' : '' }}" href="{{ route('admin.contacts.index') }}" title="{{ __('Contacts') }}">
+        {{-- <a class="fh-adm-nav-link {{ request()->routeIs('admin.contacts.*') ? 'is-active' : '' }}" href="{{ route('admin.contacts.index') }}" title="{{ __('Contacts') }}">
             <i class="bi bi-envelope-open"></i> <span>{{ __('Contacts') }}</span>
         </a>
         <a class="fh-adm-nav-link {{ request()->routeIs('admin.subscribers.*') ? 'is-active' : '' }}" href="{{ route('admin.subscribers.index') }}" title="{{ __('Subscribers') }}">
             <i class="bi bi-person-lines-fill"></i> <span>{{ __('Subscribers') }}</span>
-        </a>
+        </a> --}}
         <a class="fh-adm-nav-link {{ request()->routeIs('admin.reviews.*') ? 'is-active' : '' }}" href="{{ route('admin.reviews.index') }}" title="{{ __('Reviews') }}">
             <i class="bi bi-chat-square-text"></i> <span>{{ __('Reviews') }}</span>
         </a>
